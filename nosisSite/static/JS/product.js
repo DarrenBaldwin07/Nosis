516411ecd82ecc7e7b868695b90a6117b008246b
Darren B <68653294+Devd0@users.noreply.github.com>
2021-10-14 11:16:48 -0400

    added condition percentage progress bars

--- nosisSite/static/JS/product.js
+++ nosisSite/static/JS/product.js
@@ -3,4 +3,14 @@
 
 
 
+const percent = document.querySelectorAll('.percent');
+const progressbar = document.querySelectorAll('.red');
 
+let counter = 0;
+
+percent.forEach(i => {
+    
+    progressbar[counter].style.width = String((i.innerHTML.replace('%', '') * 1)).concat('%');
+    counter++
+    
+});
